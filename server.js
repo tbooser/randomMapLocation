8add239ca59ea34d06f2ec9cbd0f757005b51c8c
Timothy Booser <tbooser@utexas.edu>
2016-04-20 23:25:35 -0500

    Changed file structure, css, and now deployed to heroku

--- server.js
+++ server.js
@@ -3,39 +3,26 @@ var express = require('express');
 var Path = require('path');
 var routes = express.Router();
 
-//route to your index.html
+//Route to index.html
 var assetFolder = Path.resolve(__dirname, './public/');
   routes.use(express.static(assetFolder));
 
 if (process.env.NODE_ENV !== 'test') {
-//The following GET request now works but only if the catch-all 
-//route is commented out as well as routes.use below the assetFolder
-// declaration. It works but I don't think it is correct
-  
-  
-  // The Catch-all Route
-  // This is for supporting browser history pushstate.
-  // NOTE: Make sure this route is always LAST.
   routes.get('/*', function(req, res){
     res.sendFile( assetFolder + '/index.html' )
   })
 
-  // We're in development or production mode;
-  // create and run a real server.
-  var app = express();
-  // configure our server with all the middleware and and routing
+var app = express();
 
-// export our app for testing and flexibility, required by index.js
 module.exports = app;
-
-  // Mount our main router
+ 
   app.use('/', routes);
-
-  // Start the server!
   var port = process.env.PORT || 4000;
   app.listen(port);
   console.log("Listening on port", port);
+
 } else {
-  // We're in test mode; make this file importable instead.
+
   module.exports = routes;
+
 }
