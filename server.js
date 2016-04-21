55835bd61b850e4d8c1cff3ec41e33aa4d53ba90
Timothy Booser <tbooser@utexas.edu>
2016-04-20 20:41:56 -0500

    server.js changes

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var http = require('http');
 var app = express();
 
-const port= process.env.port || 5000; 
-	app.listen(port);
+const PORT= process.env.PORT || 5000; 
+	app.listen(PORT);
 //We need a function which handles requests and send response
 function handleRequest(request, response){
     response.end('It Works!! Path Hit: ' + request.url);
@@ -13,7 +13,7 @@ function handleRequest(request, response){
 var server = http.createServer(handleRequest);
 
 //Lets start our server
-server.listen(port, function(){
+server.listen(PORT, function(){
     //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", port);
+    console.log("Server listening on: http://localhost:%s", PORT);
 });
