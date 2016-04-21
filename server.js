91429e2166e3c30b79275e23a9fdd3551ba382f6
Timothy Booser <tbooser@utexas.edu>
2016-04-20 20:09:47 -0500

    added Procfile

---
+++ server.js
@@ -0,0 +1,18 @@
+
+var http = require('http');
+
+const PORT=5000; 
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
