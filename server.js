2af862a2b565f61d951e96c73da940c6b04c4c4b
Timothy Booser <tbooser@utexas.edu>
2016-04-20 20:38:09 -0500

    Setting up deployment to Heroku

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-
+var express = require('express');
 var http = require('http');
+var app = express();
 
-const PORT=5000; 
-
+const port= process.env.port || 5000; 
+	app.listen(port);
 //We need a function which handles requests and send response
 function handleRequest(request, response){
     response.end('It Works!! Path Hit: ' + request.url);
@@ -12,7 +13,7 @@ function handleRequest(request, response){
 var server = http.createServer(handleRequest);
 
 //Lets start our server
-server.listen(PORT, function(){
+server.listen(port, function(){
     //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+    console.log("Server listening on: http://localhost:%s", port);
 });
